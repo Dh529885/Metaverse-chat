191c6136607fddf82255a11263f86502aaa53c69
Dh529885 <89321290+Dh529885@users.noreply.github.com>
2021-12-22 21:08:53 -0500

    add Avatar, ChangeUsername, and Header components

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,23 @@
 import Head from "next/head";
 import Login from "../components/Login";
 import { useMoralis } from "react-moralis";
+import Header from "../components/Header";
 
 export default function Home() {
   const { isAuthenticated, logout } = useMoralis();
   if (!isAuthenticated) return <Login />;
 
   return (
-    <div className="h-screen">
+    <div className="h-screen overflow-y-scroll bg-gradient-to-b from-black to-fuchsia-500 overflow-hidden">
       <Head>
         <title>Metaverse</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <h1>Welcome to app</h1>
-      <button onClick={logout}>Logout</button>
+
+      <div className=" max-w-screen-2xl mx-auto">
+        <Header />
+        <button onClick={logout}>Logout</button>
+      </div>
     </div>
   );
 }
